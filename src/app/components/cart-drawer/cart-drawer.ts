3c6654b9dc77e5f2aef2ae7bd3805b06afe2fa47
vijay-fs <vijaykarthik9868@gmail.com>
2025-07-15 02:04:52 +0530

    used routerLink instead of href methods

--- src/app/components/cart-drawer/cart-drawer.ts
+++ src/app/components/cart-drawer/cart-drawer.ts
@@ -1,29 +1,15 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-cart-drawer',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './cart-drawer.html',
   styleUrl: './cart-drawer.scss'
 })
 export class CartDrawer {
   cartService = inject(CartService);
   router = inject(Router);
-  
-  navigateToCheckout() {
-    // Close the cart drawer
-    const cartDrawer = document.getElementById('offcanvasCart');
-    if (cartDrawer) {
-      const bsOffcanvas = (window as any).bootstrap?.Offcanvas?.getInstance(cartDrawer);
-      if (bsOffcanvas) {
-        bsOffcanvas.hide();
-      }
-    }
-    
-    // Navigate to checkout
-    this.router.navigate(['/checkout']);
-  }
 }
