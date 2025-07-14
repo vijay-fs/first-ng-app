7fd0ce1849927b4c2d7ae23b5a3a7b848667c32e
vijay-fs <vijaykarthik9868@gmail.com>
2025-07-15 00:16:02 +0530

    added view cart button in wishlist drawer

--- src/app/components/wishlist-drawer/wishlist-drawer.ts
+++ src/app/components/wishlist-drawer/wishlist-drawer.ts
@@ -17,4 +17,24 @@ export class WishlistDrawer {
     this.cartService.addToCart(item);
     this.wishlistService.removeFromWishlist(item.id);
   }
+
+  openCartDrawer() {
+    // Close wishlist drawer first
+    const wishlistDrawer = document.getElementById('offcanvasWishlist');
+    if (wishlistDrawer) {
+      const bsOffcanvas = (window as any).bootstrap?.Offcanvas?.getInstance(wishlistDrawer);
+      if (bsOffcanvas) {
+        bsOffcanvas.hide();
+      }
+    }
+
+    // Open cart drawer after a short delay to allow wishlist to close
+    setTimeout(() => {
+      const cartDrawer = document.getElementById('offcanvasCart');
+      if (cartDrawer) {
+        const bsOffcanvas = new (window as any).bootstrap.Offcanvas(cartDrawer);
+        bsOffcanvas.show();
+      }
+    }, 300);
+  }
 }
