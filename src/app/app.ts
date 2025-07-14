4c7e44c6a575b677ac6b635513e9980dd80e3612
vijay-fs <vijaykarthik9868@gmail.com>
2025-07-15 01:02:09 +0530

    scroll to top feature added !

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
 import { Footer } from './components/footer/footer';
 import { ToastComponent } from './components/toast/toast';
+import { ScrollToTop } from './components/scroll-to-top/scroll-to-top';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer, ToastComponent],
+  imports: [RouterOutlet, Header, Footer, ToastComponent, ScrollToTop],
   template: `
     <app-header />
     <main>
@@ -14,6 +15,7 @@ import { ToastComponent } from './components/toast/toast';
     </main>
     <app-footer />
     <app-toast />
+    <app-scroll-to-top />
   `,
   styles: [`
     main {
